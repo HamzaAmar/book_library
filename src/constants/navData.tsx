f722a95153182b1c07aa9c6c3139d41669e59602
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-11-05 21:25:54 +0100

    feat: add nav data

---
+++ src/constants/navData.tsx
@@ -0,0 +1,121 @@
+import { NavType } from "@types/nav";
+
+export const NAV_DATA: NavType[] = [
+  {
+    id: 1,
+    title: "Shop",
+    link: "/shop",
+  },
+  {
+    id: 2,
+    title: "Authors",
+    link: "/author",
+  },
+  {
+    id: 3,
+    title: "Categories",
+    link: "/category",
+  },
+  {
+    id: 4,
+    title: "About Us",
+    link: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    link: "/contact",
+  },
+];
+
+export const CUSTOM_AREA_NAV: NavType[] = [
+  {
+    id: 1,
+    title: "My Account",
+    link: "/account",
+  },
+  {
+    id: 2,
+    title: "Orders",
+    link: "/orders",
+  },
+  {
+    id: 3,
+    title: "Tracking List",
+    link: "/tracking",
+  },
+  {
+    id: 4,
+    title: "Terms",
+    link: "/terms",
+  },
+  {
+    id: 5,
+    title: "Privacy Policy",
+    link: "/privacy",
+  },
+  {
+    id: 6,
+    title: "FAQ",
+    link: "/faq",
+  },
+];
+export const CATEGORIES_NAV: NavType[] = [
+  {
+    id: 1,
+    title: "Business",
+    link: "/business",
+  },
+  {
+    id: 2,
+    title: "Children",
+    link: "/children",
+  },
+  {
+    id: 3,
+    title: "Technology",
+    link: "/technology",
+  },
+  {
+    id: 4,
+    title: "E_Book",
+    link: "/E_Book",
+  },
+  {
+    id: 5,
+    title: "Fiction",
+    link: "/fiction",
+  },
+  {
+    id: 6,
+    title: "Toys And Games",
+    link: "/toys_games",
+  },
+];
+export const SOCIAL_MEDIA_NAV: NavType[] = [
+  {
+    id: 1,
+    title: "Facebook",
+    link: "/Facebook",
+  },
+  {
+    id: 2,
+    title: "Instagram",
+    link: "/Instagram",
+  },
+  {
+    id: 3,
+    title: "Twitter",
+    link: "/Twitter",
+  },
+  {
+    id: 4,
+    title: "Linkdin",
+    link: "/Linkdin",
+  },
+  {
+    id: 5,
+    title: "Dribble",
+    link: "/Dribble",
+  },
+];
