549381d3cc3dfcbddb90d7e434bb5f585a252709
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-11-05 22:21:26 +0100

    feat: wrap all _app in uiContext to triger open and close model and sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "keen-slider/keen-slider.min.css";
 import type { AppProps } from "next/app";
+import { UIContext } from "@context/UIContext";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <UIContext>
+      <Component {...pageProps} />
+    </UIContext>
+  );
 }
-
 export default MyApp;
